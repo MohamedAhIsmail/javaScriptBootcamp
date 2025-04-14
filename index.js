e3d266cc052dd171ae7545e08c8351433213e8cb
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-04-14 22:31:49 +0200

    js

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Hello Server");
+
+
+for(let i = 0; i <= 100; i++){
+  console.log(i);
+  
+}
--- js/script.js
+++ js/script.js
@@ -11,10 +11,10 @@
 // console.log(num);
 // console.log(++num);
 
-console.log(2 * "4");
-console.log(8 / "2");
-console.log("8" % "2");
-console.log("2" - "2");
+// console.log(2 * "4");
+// console.log(8 / "2");
+// console.log("8" % "2");
+// console.log("2" - "2");
 
 // let num = "12"
 
@@ -36,24 +36,59 @@ form.addEventListener("submit", (e) => {
 
 // console.log(typeof +name);
 
-console.log(-"-asdasd");
-console.log(-"+123");
+// console.log(-"-asdasd");
+// console.log(-"+123");
 
-console.log(Number("ali"));
-console.log(Number(""));
-console.log(Number("         "));
+// console.log(Number("ali"));
+// console.log(Number(""));
+// console.log(Number("         "));
 
-console.log(Number(undefined));
+// console.log(Number(undefined));
 
-console.log(typeof undefined);
+// console.log(typeof undefined);
 
-console.log(typeof +undefined);
-console.log(Number(+undefined));
-console.log(true - false);
-console.log(true);
-console.log(15 == 15);
-console.log(15 != 15);
-console.log(15 !== "15");
+// console.log(typeof +undefined);
+// console.log(Number(+undefined));
+// console.log(true - false);
+// console.log(true);
+// console.log(15 == 15);
+// console.log(15 != 15);
+// console.log(15 !== "15");
 
-console.log(!(10 > 10));
+// console.log(!(10 > 10));
+
+// let i = 0
+
+// for (; i <= 100; ) {
+//   console.log(i);
+//   i++
+
+// }
+
+// var cartona ="";
+// for (let i = 1990; i < 2025; i++) {
+//   console.log(i);
+
+//   cartona += "<option>" + i + "</option>"
+// }
+
+// document.getElementById("select").innerHTML = cartona
+// console.log(cartona);
+
+// for (;;){
+//   console.log("Hello");
+
+// }
+
+// function sum(...numbers) {
+//   let res = 0;
+//   for (let i = 0; i < numbers.length; i++) {
+//     res += numbers[i];
+//   }
+
+//   return res;
+// }
+
+// console.log(sum(1, 1, 1, 1));
+
 
