63287d60e3aed35c6b3d8e97be10655760d96a83
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-04-17 03:02:29 +0200

    added search

--- cruds/main.js
+++ cruds/main.js
@@ -2,6 +2,7 @@ var pName = document.getElementById("productName");
 var pPrice = document.getElementById("productPrice");
 var pCat = document.getElementById("productCat");
 var pDesc = document.getElementById("productDesc");
+var searchInput = document.getElementById("search");
 var btn = document.getElementById("btn");
 
 var productsList = [];
@@ -25,23 +26,23 @@ btn.addEventListener("click", function addProduct() {
   window.localStorage.setItem("Products", JSON.stringify(productsList));
 });
 
-function displayProducts() {
-  var item = "";
-  for (var i = 0; i < productsList.length; i++) {
-    item += `
-        <tr>
-            <td>${i + 1}</td>
-            <td>${productsList[i].productName}</td>
-            <td>${productsList[i].productPrice}</td>
-            <td>${productsList[i].productCat}</td>
-            <td>${productsList[i].productDesc}</td>
-            <td><button onclick="updateProduct(${i})" class="btn btn-outline-warning">Update</button></td>
-            <td><button onclick="deleteProduct(${i})" class="btn btn-outline-danger">Delete</button></td>
-          </tr>
-    `;
-  }
-  document.getElementById("tbody").innerHTML = item;
-}
+// function displayProducts() {
+//   var item = "";
+//   for (var i = 0; i < productsList.length; i++) {
+//     item += `
+//         <tr>
+//             <td>${i + 1}</td>
+//             <td>${productsList[i].productName}</td>
+//             <td>${productsList[i].productPrice}</td>
+//             <td>${productsList[i].productCat}</td>
+//             <td>${productsList[i].productDesc}</td>
+//             <td><button onclick="updateProduct(${i})" class="btn btn-outline-warning">Update</button></td>
+//             <td><button onclick="deleteProduct(${i})" class="btn btn-outline-danger">Delete</button></td>
+//           </tr>
+//     `;
+//   }
+//   document.getElementById("tbody").innerHTML = item;
+// }
 
 function deleteProduct(i) {
   productsList.splice(i, 1);
@@ -61,6 +62,34 @@ function updateProduct(i) {
   pPrice.value = productsList[i].productPrice;
   pCat.value = productsList[i].productCat;
   pDesc.value = productsList[i].productDesc;
+}
+
+function displayProducts() {
+  var item = "";
+  for (var i = 0; i < productsList.length; i++) {
+    if (
+      productsList[i].productName
+        .toLowerCase()
+        .startsWith(searchInput.value.toLowerCase()) ||
+      productsList[i].productCat
+        .toLowerCase()
+        .startsWith(searchInput.value.toLowerCase())
+    ) {
+      item += `
+          <tr>
+            <td>${i + 1}</td>
+            <td>${productsList[i].productName}</td>
+            <td>${productsList[i].productPrice}</td>
+            <td>${productsList[i].productCat}</td>
+            <td>${productsList[i].productDesc}</td>
+            <td><button onclick="updateProduct(${i})" class="btn btn-outline-warning">Update</button></td>
+            <td><button onclick="deleteProduct(${i})" class="btn btn-outline-danger">Delete</button></td>
+          </tr>
+      `;
+    }
+  }
+
+  document.getElementById("tbody").innerHTML = item;
 }
 
 function emptyInputs() {
