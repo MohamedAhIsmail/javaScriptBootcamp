6176b7e475806d01e3c2871f282294e0ef670ab3
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-04-19 23:33:22 +0200

    added cart page and display counter in all pages

--- eCommerce/cart.js
+++ eCommerce/cart.js
@@ -1,4 +1,31 @@
-// import { displayCounter } from "./main.js";
+let cartContainer = document.querySelector(".products");
+let cartItems = JSON.parse(localStorage.getItem("cart")) || [];
+let cartCounter = document.querySelector(".cart");
 
+function displayCartItems() {
+  let content = "";
 
-// displayCounter()
+  if (cartItems.length === 0) {
+    content = "<p>Your cart is empty.</p>";
+  } else {
+    for (let i = 0; i < cartItems.length; i++) {
+      content += `
+        <div class="product">
+          <img src="${cartItems[i].img}" alt="${cartItems[i].name}">
+          <h2>${cartItems[i].name}</h2>
+          <p>$${cartItems[i].price}</p>
+        </div>
+      `;
+    }
+  }
+
+  cartContainer.innerHTML = content;
+}
+
+displayCartItems();
+
+function displayCounter() {
+  cartCounter.innerHTML = cartItems.length;
+}
+
+displayCounter();
