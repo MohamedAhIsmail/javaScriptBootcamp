030db902f0b7a21fa1d4a91037b9c986f7ed9e21
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-04-20 23:49:52 +0200

    handel addToCart to add product one time

--- eCommerce/details.js
+++ eCommerce/details.js
@@ -4,28 +4,38 @@ let cartCounter = document.querySelector(".cart");
 function displayCounter() {
   cartCounter.innerHTML = cartItems.length;
 }
-
 displayCounter();
 
 let detailsContainer = document.getElementById("details");
 
 function getDetails() {
-  singleProduct = JSON.parse(window.localStorage.getItem("singleProduct"));
+  const singleProduct = JSON.parse(localStorage.getItem("singleProduct"));
   console.log(singleProduct);
-  let details = ``;
 
-  details = `
-          <div class="image">
-            <img src="${singleProduct.img}" alt="">
-          </div>
-          <div class="info">
-            <h2 class="title">${singleProduct.name}</h2>
-            <p class="price">$${singleProduct.price}</p>
-          </div>
-  
+  let details = `
+    <div class="image">
+      <img src="${singleProduct.img}" alt="">
+    </div>
+    <div class="info">
+      <h2 class="title">${singleProduct.name}</h2>
+      <p class="price">$${singleProduct.price}</p>
+      <p class="desc">${singleProduct.desc || "No description available."}</p>
+      <button class="btn addToCart">Add to Cart</button>
+    </div>
   `;
 
-  detailsContainer.innerHTML = details
+  detailsContainer.innerHTML = details;
+
+  document.querySelector(".addToCart").addEventListener("click", function () {
+    const alreadyInCart = cartItems.find(
+      (item) => item.name === singleProduct.name
+    );
+    if (!alreadyInCart) {
+      cartItems.push(singleProduct);
+      localStorage.setItem("cart", JSON.stringify(cartItems));
+      cartCounter.innerHTML = cartItems.length;
+    }
+  });
 }
 
 getDetails();
--- eCommerce/main.js
+++ eCommerce/main.js
@@ -2,61 +2,73 @@ let products = [
   {
     name: "Addaptor Laptop 2025",
     img: "./images/add.png",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem unde suscipit nemo distinctio, repellendus quas. Pariatur illo sequi reiciendis harum corrupti! Molestias, nisi quia similique at, error explicabo minus exercitationem qui deserunt enim molestiae atque ipsam sapiente quod quasi et sint cupiditate perspiciatis libero neque ducimus maxime reiciendis excepturi. Laborum!",
     price: 1000,
   },
   {
     name: "PC Case 2025",
     img: "./images/case.png",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem unde suscipit nemo distinctio, repellendus quas. Pariatur illo sequi reiciendis harum corrupti! Molestias, nisi quia similique at, error explicabo minus exercitationem qui deserunt enim molestiae atque ipsam sapiente quod quasi et sint cupiditate perspiciatis libero neque ducimus maxime reiciendis excepturi. Laborum!",
     price: 2000,
   },
   {
     name: "Asus gaming Laptop 2025",
     img: "./images/lap-asus.png",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem unde suscipit nemo distinctio, repellendus quas. Pariatur illo sequi reiciendis harum corrupti! Molestias, nisi quia similique at, error explicabo minus exercitationem qui deserunt enim molestiae atque ipsam sapiente quod quasi et sint cupiditate perspiciatis libero neque ducimus maxime reiciendis excepturi. Laborum!",
     price: 3000,
   },
   {
     name: "MSI Laptop 2025",
     img: "./images/lapmsi.png",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem unde suscipit nemo distinctio, repellendus quas. Pariatur illo sequi reiciendis harum corrupti! Molestias, nisi quia similique at, error explicabo minus exercitationem qui deserunt enim molestiae atque ipsam sapiente quod quasi et sint cupiditate perspiciatis libero neque ducimus maxime reiciendis excepturi. Laborum!",
     price: 4000,
   },
   {
     name: "Black Laptop 2025",
     img: "./images/laptop-black.png",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem unde suscipit nemo distinctio, repellendus quas. Pariatur illo sequi reiciendis harum corrupti! Molestias, nisi quia similique at, error explicabo minus exercitationem qui deserunt enim molestiae atque ipsam sapiente quod quasi et sint cupiditate perspiciatis libero neque ducimus maxime reiciendis excepturi. Laborum!",
     price: 5000,
   },
   {
     name: "Zen Laptop 2025",
     img: "./images/laptop-zen.png",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem unde suscipit nemo distinctio, repellendus quas. Pariatur illo sequi reiciendis harum corrupti! Molestias, nisi quia similique at, error explicabo minus exercitationem qui deserunt enim molestiae atque ipsam sapiente quod quasi et sint cupiditate perspiciatis libero neque ducimus maxime reiciendis excepturi. Laborum!",
     price: 6000,
   },
   {
     name: "Lenovo Laptop 2025",
     img: "./images/laptop.png",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem unde suscipit nemo distinctio, repellendus quas. Pariatur illo sequi reiciendis harum corrupti! Molestias, nisi quia similique at, error explicabo minus exercitationem qui deserunt enim molestiae atque ipsam sapiente quod quasi et sint cupiditate perspiciatis libero neque ducimus maxime reiciendis excepturi. Laborum!",
     price: 7000,
   },
   {
     name: "Silver Laptop 2025",
     img: "./images/laptopo.png",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem unde suscipit nemo distinctio, repellendus quas. Pariatur illo sequi reiciendis harum corrupti! Molestias, nisi quia similique at, error explicabo minus exercitationem qui deserunt enim molestiae atque ipsam sapiente quod quasi et sint cupiditate perspiciatis libero neque ducimus maxime reiciendis excepturi. Laborum!",
     price: 8000,
   },
   {
     name: "Microsoft Laptop 2025",
     img: "./images/new-lap.png",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem unde suscipit nemo distinctio, repellendus quas. Pariatur illo sequi reiciendis harum corrupti! Molestias, nisi quia similique at, error explicabo minus exercitationem qui deserunt enim molestiae atque ipsam sapiente quod quasi et sint cupiditate perspiciatis libero neque ducimus maxime reiciendis excepturi. Laborum!",
     price: 9000,
   },
   {
     name: "PC Bundle RGB",
     img: "./images/pc-build.png",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem unde suscipit nemo distinctio, repellendus quas. Pariatur illo sequi reiciendis harum corrupti! Molestias, nisi quia similique at, error explicabo minus exercitationem qui deserunt enim molestiae atque ipsam sapiente quod quasi et sint cupiditate perspiciatis libero neque ducimus maxime reiciendis excepturi. Laborum!",
     price: 10000,
   },
   {
     name: "Gaming PC 2025",
     img: "./images/pc.png",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem unde suscipit nemo distinctio, repellendus quas. Pariatur illo sequi reiciendis harum corrupti! Molestias, nisi quia similique at, error explicabo minus exercitationem qui deserunt enim molestiae atque ipsam sapiente quod quasi et sint cupiditate perspiciatis libero neque ducimus maxime reiciendis excepturi. Laborum!",
     price: 11000,
   },
   {
     name: "RAM 2 X 16 2025",
     img: "./images/ram.png",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem unde suscipit nemo distinctio, repellendus quas. Pariatur illo sequi reiciendis harum corrupti! Molestias, nisi quia similique at, error explicabo minus exercitationem qui deserunt enim molestiae atque ipsam sapiente quod quasi et sint cupiditate perspiciatis libero neque ducimus maxime reiciendis excepturi. Laborum!",
     price: 12000,
   },
 ];
@@ -96,11 +108,13 @@ displayProducts();
 searchInput.addEventListener("input", displayProducts);
 
 function addToCart(i) {
-  cart.push(products[i]);
-  console.log(cart);
-
-  cartCounter.innerHTML = cart.length;
-  saveData();
+  const alreadyInCart = cart.find((item) => item.name === products[i].name);
+  if (!alreadyInCart) {
+    cart.push(products[i]);
+    console.log(cart);
+    cartCounter.innerHTML = cart.length;
+    saveData();
+  }
 }
 
 function saveData() {
