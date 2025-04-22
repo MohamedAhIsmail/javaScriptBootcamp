2cedb6e1555d5b7e209479852c297bd947065d18
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-04-22 23:49:11 +0200

    onclick and addevent

--- Dom/script.js
+++ Dom/script.js
@@ -2,35 +2,60 @@
 
 // console.log(test);
 
-
-
 // document.addEventListener("click", (e)=>{
 //   e.target.style.color = "red"
 // })
 
-
-let childs = document.querySelectorAll(".child")
+let childs = document.querySelectorAll(".child");
 
 console.log(childs);
 
+for (let i = 0; i < childs.length; i++) {
+  // childs[i].addEventListener("click", function(e){
+  //   e.target.classList.add("new")
+  // })
 
+  // childs[i].addEventListener("mousedown", function(){
+  //   childs[i].style.height = "300px"
+  // })
+  childs[i].addEventListener("dblclick", function () {
+    childs[i].style.height = "300px";
+  });
+  childs[i].addEventListener("mouseup", function () {
+    childs[i].style.height = "50px";
+  });
+}
 
-  for(let i = 0; i< childs.length; i++){
-    // childs[i].addEventListener("click", function(e){
-    //   e.target.classList.add("new")
-    // })
+document.addEventListener("contextmenu", (e) => {
+  // e.preventDefault()
+});
 
-    // childs[i].addEventListener("mousedown", function(){
-    //   childs[i].style.height = "300px"
-    // })
-    childs[i].addEventListener("dblclick", function(){
-      childs[i].style.height = "300px"
-    })
-    childs[i].addEventListener("mouseup", function(){
-      childs[i].style.height = "50px"
-    })
+let btn = document.querySelector("button");
+console.log(btn);
+
+// btn.onclick = function () {
+//   document.querySelector("h3").style.color = "tomato";
+// };
+
+// btn.onclick = function () {
+//   console.log("hllow");
+// };
+
+btn.addEventListener("click", function () {
+  console.log("hey");
+});
+
+btn.addEventListener("click", () => {
+  document.querySelector("h3").style.color = "green";
+});
+
+let grad = document.querySelector(".grad");
+
+console.log(grad);
+
+window.onscroll = function () {
+  console.log(window.scrollY);
+  if (window.scrollY == 100) {
+    grad.style.width = "100%";
   }
-
-  document.addEventListener("contextmenu", (e)=>{
-    // e.preventDefault()
-  })
+};
