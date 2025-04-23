9a3404f95bcdbfa5a583437ade6744ced6a170db
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-04-23 21:08:59 +0200

    promises

--- Api/fetch.js
+++ Api/fetch.js
@@ -1,36 +1,91 @@
-function getPizza(callback) {
-  let request = new XMLHttpRequest();
-  request.open("GET", "https://forkify-api.herokuapp.com/api/search?q=pizza");
-  request.send();
-  request.addEventListener("readystatechange", () => {
-    if (request.readyState == 4) {
-      console.log("Pizza", JSON.parse(request.response));
-      callback();
-    }
+// function getPizza(callback) {
+//   let request = new XMLHttpRequest();
+//   request.open("GET", "https://forkify-api.herokuapp.com/api/search?q=pizza");
+//   request.send();
+//   request.addEventListener("readystatechange", () => {
+//     if (request.readyState == 4) {
+//       console.log("Pizza", JSON.parse(request.response).recipes);
+//       callback();
+//     }
+//   });
+// }
+
+// function getPasta(callback) {
+//   let request = new XMLHttpRequest();
+//   request.open("GET", "https://forkify-api.herokuapp.com/api/search?q=pasta");
+//   request.send();
+//   request.addEventListener("readystatechange", () => {
+//     if (request.readyState == 4) {
+//       console.log("Pasta", JSON.parse(request.response).recipes);
+//       callback();
+//     }
+//   });
+// }
+
+// function getCorn(callback) {
+//   let request = new XMLHttpRequest();
+//   request.open("GET", "https://forkify-api.herokuapp.com/api/search?q=corn");
+//   request.send();
+//   request.addEventListener("readystatechange", () => {
+//     if (request.readyState == 4) {
+//       console.log("Corn", JSON.parse(request.response).recipes);
+//       callback();
+//     }
+//   });
+// }
+
+// function done() {
+//   console.log("All Done");
+// }
+
+// getPizza(function () {
+//   getPasta(function () {
+//     getCorn(done);
+//   });
+// });
+// getPasta();
+// getCorn();
+// done();
+
+function getPizza() {
+  return new Promise(function (callback) {
+    let request = new XMLHttpRequest();
+    request.open("GET", "https://forkify-api.herokuapp.com/api/search?q=pizza");
+    request.send();
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState == 4) {
+        console.log("Pizza", JSON.parse(request.response).recipes);
+        callback();
+      }
+    });
   });
 }
 
-function getPasta(callback) {
-  let request = new XMLHttpRequest();
-  request.open("GET", "https://forkify-api.herokuapp.com/api/search?q=pasta");
-  request.send();
-  request.addEventListener("readystatechange", () => {
-    if (request.readyState == 4) {
-      console.log("Pasta", JSON.parse(request.response));
-      callback();
-    }
+function getPasta() {
+  return new Promise(function (callback) {
+    let request = new XMLHttpRequest();
+    request.open("GET", "https://forkify-api.herokuapp.com/api/search?q=pasta");
+    request.send();
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState == 4) {
+        console.log("Pasta", JSON.parse(request.response).recipes);
+        callback();
+      }
+    });
   });
 }
 
-function getCorn(callback) {
-  let request = new XMLHttpRequest();
-  request.open("GET", "https://forkify-api.herokuapp.com/api/search?q=corn");
-  request.send();
-  request.addEventListener("readystatechange", () => {
-    if (request.readyState == 4) {
-      console.log("Corn", JSON.parse(request.response));
-      callback();
-    }
+function getCorn() {
+  return new Promise(function (callback) {
+    let request = new XMLHttpRequest();
+    request.open("GET", "https://forkify-api.herokuapp.com/api/search?q=corn");
+    request.send();
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState == 4) {
+        console.log("Corn", JSON.parse(request.response).recipes);
+        callback();
+      }
+    });
   });
 }
 
@@ -38,11 +93,4 @@ function done() {
   console.log("All Done");
 }
 
-getPizza(function () {
-  getPasta(function(){
-    getCorn(done)
-  });
-});
-// getPasta();
-// getCorn();
-// done();
+getPizza().then(getPasta).then(getCorn).then(done);
