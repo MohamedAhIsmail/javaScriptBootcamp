001db719771b782b60c8eb25fff6d4f80dfc1762
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-04-23 21:48:28 +0200

    GET method ==> fetch

--- Api/Fetch/fetch.js
+++ Api/Fetch/fetch.js
@@ -1,34 +1,53 @@
 // console.log("fetch");
 
-async function getPizza() {
+// async function getPizza() {
+//   let data = await fetch(
+//     "https://forkify-api.herokuapp.com/api/search?q=pizza"
+//   );
+
+//   let result = await data.json();
+//   console.log("Pizza", result.recipes);
+// }
+
+// async function getPasta() {
+//   let data = await fetch(
+//     "https://forkify-api.herokuapp.com/api/search?q=pasta"
+//   );
+
+//   let result = await data.json();
+//   console.log("Pasta", result.recipes);
+// }
+
+// async function getCorn() {
+//   let data = await fetch("https://forkify-api.herokuapp.com/api/search?q=corn");
+
+//   let result = await data.json();
+//   console.log("Corn", result.recipes);
+// }
+
+// async function printAll() {
+//   await getPizza();
+//   await getPasta();
+//   await getCorn();
+// }
+
+// printAll();
+
+async function getRecipe(meal) {
   let data = await fetch(
-    "https://forkify-api.herokuapp.com/api/search?q=pizza"
+    `https://forkify-api.herokuapp.com/api/search?q=${meal}`,
+    {
+      method: "GET",
+    }
   );
-
   let result = await data.json();
-  console.log("Pizza", result.recipes);
-}
-
-async function getPasta() {
-  let data = await fetch(
-    "https://forkify-api.herokuapp.com/api/search?q=pasta"
-  );
-
-  let result = await data.json();
-  console.log("Pasta", result.recipes);
-}
-
-async function getCorn() {
-  let data = await fetch("https://forkify-api.herokuapp.com/api/search?q=corn");
-
-  let result = await data.json();
-  console.log("Corn", result.recipes);
+  console.log(`${meal}`, result.recipes);
 }
 
 async function printAll() {
-  await getPizza();
-  await getPasta();
-  await getCorn();
+  await getRecipe("pizza");
+  await getRecipe("pasta");
+  await getRecipe("corn");
 }
 
 printAll();
