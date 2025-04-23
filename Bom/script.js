8ec8da5947433f599634728392c7e7a5c484964d
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-04-23 05:13:25 +0200

    simple counter + & -

--- Bom/script.js
+++ Bom/script.js
@@ -12,9 +12,37 @@ btn.addEventListener("click", function () {
 });
 
 let y = setInterval(() => {
-  document.querySelector("div").innerHTML += `${1}`
+  document.querySelector("div").innerHTML =
+    +document.querySelector("div").innerHTML + 1;
 }, 1000);
 
-btn.addEventListener("click", function() {
-  clearInterval(y)
-})
+// console.log(+document.querySelector("div").innerHTML +1);
+
+btn.addEventListener("click", function () {
+  clearInterval(y);
+});
+
+let minus = document.querySelector(".min");
+let plus = document.querySelector(".plus");
+let result = document.querySelector(".result");
+
+console.log(minus, plus, result);
+
+// setInterval(()=>{
+//   result.innerHTML = +result.innerHTML + 1
+// }, 1000)
+
+plus.addEventListener("click", () => {
+  result.innerHTML = +result.innerHTML + 1;
+  minus.removeAttribute("disabled")
+});
+
+
+minus.addEventListener("click", () => {
+  result.innerHTML = +result.innerHTML - 1;
+  if(result.innerHTML == 0){
+    minus.setAttribute("disabled", "disabled")
+  } else {
+    minus.removeAttribute("disabled")
+  }
+});
