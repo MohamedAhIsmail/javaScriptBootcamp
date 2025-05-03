762dae46f2a9168967680e85e66b9ce3750b174e
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-05-04 02:53:18 +0300

    Assignments_71_78

--- jsCourseTasks/Assignments/Assignments_71_78/script.js
+++ jsCourseTasks/Assignments/Assignments_71_78/script.js
@@ -1,9 +1,88 @@
-let removeChars = ["E", "@", "@", "L", "Z", "@", "@", "E", "R", "@", "O"];
+// let removeChars = ["E", "@", "@", "L", "Z", "@", "@", "E", "R", "@", "O"];
 
-let newrem = removeChars.filter((ele) => {
-  return ele !== "@"
-}).reduce((acc, curr)=> {
-  return acc + curr
-});
+// let newrem = removeChars.filter((ele) => {
+//   return ele !== "@"
+// }).reduce((acc, curr)=> {
+//   return acc + curr
+// });
 
-console.log(newrem);
+// console.log(newrem);
+
+// Task 1
+
+let mix = [1, 2, 3, "E", 4, "l", "z", "e", "r", 5, "o"];
+
+let newArr = mix
+  .map((ele) => {
+    return typeof ele != "number" ? ele : "";
+  })
+  .reduce((acc, curr) => {
+    return acc + curr;
+  });
+
+console.log(newArr);
+
+// Elzero
+
+console.log("#".repeat(20));
+
+// Task 2
+
+let myString = "EElllzzzzzzzeroo";
+
+let newStr = myString
+  .split("")
+  .filter((ele, index, arr) => {
+    return arr.indexOf(ele) === index;
+  })
+  .join("");
+
+console.log(newStr);
+
+// Elzero
+
+console.log("#".repeat(20));
+
+// Task 3
+
+let myArray = ["E", "l", "z", ["e", "r"], "o"];
+let newArray = myArray
+  .reduce((acc, curr) => {
+    return acc + curr;
+  })
+  .replace(",", "");
+console.log(newArray);
+
+// Elzero
+
+console.log("#".repeat(20));
+
+// Task 4
+
+let numsAndStrings = [1, 10, -10, -20, 5, "A", 3, "B", "C"];
+
+let newNumsStr = numsAndStrings
+  .filter((ele) => {
+    return typeof ele === "number";
+  })
+  .map((ele) => {
+    return -ele;
+  });
+
+console.log(newNumsStr);
+
+// [-1, -10, 10, 20, -5, -3]
+
+console.log("#".repeat(20));
+
+// Task 5
+
+let nums = [2, 12, 11, 5, 10, 1, 99];
+
+let lastNums = nums.reduce((acc, curr) => {
+  return curr % 2 === 0 ? curr * acc : acc + curr;
+}, 1);
+
+console.log(lastNums);
+
+// 500
