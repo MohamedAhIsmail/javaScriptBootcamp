9e6619539f2c35718df776a29381157e2fbc9d60
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-05-06 06:59:56 +0300

    Assignments_94_101

--- jsCourseTasks/Assignments/Assignments_94_101/script.js
+++ jsCourseTasks/Assignments/Assignments_94_101/script.js
@@ -1,6 +1,48 @@
-let myP = document.querySelector(".my");
+// let myP = document.querySelector(".my");
 
-myP.addEventListener("click", () => {
-  let newP = myP.cloneNode(true);
-  document.body.appendChild(newP)
-});
+// myP.addEventListener("click", () => {
+//   let newP = myP.cloneNode(true);
+//   document.body.appendChild(newP)
+// });
+
+
+
+
+
+// Task 3
+
+// let p = document.querySelector("p");
+// p.remove();
+
+// let mainDiv = document.querySelector(".our-element");
+
+// let start = document.createElement("div");
+// start.className = "start";
+// start.setAttribute("title", "Start Element");
+// start.setAttribute("data-value", "Start");
+// start.innerText = "Start";
+
+// let end = document.createElement("div");
+// end.className = "end"
+// end.textContent = "End"
+// end.setAttribute("title", "End Element")
+// end.setAttribute("data-value", "End")
+// mainDiv.before(start);
+// mainDiv.after(end)
+
+
+
+
+// task 4
+
+// let theDiv = document.querySelector("div")
+// console.log(theDiv.childNodes[4].textContent.trim());
+
+
+
+// task 5
+
+document.addEventListener("click", (e)=>{
+  console.log(`This Is Element Type: ${e.target.nodeName}`);
+  
+})
